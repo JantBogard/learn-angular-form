a9e303c2747671c2e5f6c92646960ff623cc112f
JantBogard <joancenkonga@gmail.com>
2022-11-10 06:07:09 +0100

    error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
@@ -7,15 +7,32 @@ import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('confirmation', { static: true }) public el!: ElementRef<HTMLInputElement>;
   public form: FormGroup = new FormGroup({
     nom: new FormControl('', [this.validatorPaul, Validators.required], this.validatorAsync),
     email: new FormControl(''),
-    password: new FormControl('')
+    password: new FormControl('', this.passwordMatch.bind(this))
   });
+
+  get nom() {
+    return this.form.get('nom');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
 
   validatorPaul(formControl: AbstractControl): { [s: string]: boolean } | null {
     if (formControl.value == 'paul') {
       return { notPaul: true }
+    } else {
+      return null;
+    }
+  }
+
+  passwordMatch(formControl: AbstractControl): { [s: string]: boolean } | null {
+    if (formControl.value !== this.el.nativeElement.value) {
+      return { notMatching: true }
     } else {
       return null;
     }
