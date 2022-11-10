aad8ff35e9d1e6e3ca6330f45e242a021f164805
JantBogard <joancenkonga@gmail.com>
2022-11-10 05:09:19 +0100

    add validator in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn-form';
+  public form: FormGroup = new FormGroup({
+    nom: new FormControl('', [this.validatorPaul, Validators.required], this.validatorAsync),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  validatorPaul(formControl: AbstractControl): { [s: string]: boolean } | null {
+    if (formControl.value == 'paul') {
+      return { notPaul: true }
+    } else {
+      return null;
+    }
+  }
+
+  validatorAsync(formControl: AbstractControl): Promise<{ [s: string]: boolean } | null> {
+    return new Promise((resolve, rejet) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 50000);
+    })
+  }
+
+  submit(): void {
+    console.log(this.form);
+  }
 }
