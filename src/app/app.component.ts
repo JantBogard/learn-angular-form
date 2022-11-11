cc52f9acfa8f8a6d9a525e8c8d1a8f1fd21e996f
JantBogard <joancenkonga@gmail.com>
2022-11-11 10:48:31 +0100

    add formArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,49 +7,27 @@ import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  @ViewChild('confirmation', { static: true }) public el!: ElementRef<HTMLInputElement>;
   public form: FormGroup;
 
-  get nom() {
-    return this.form.get('nom');
-  }
-
-  get password() {
-    return this.form.get('login').get('password');
+  get hobbies() {
+    return this.form.get('hobbies') as FormArray;
   }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      login: new FormGroup({
-        email: new FormControl(''),
-        password: new FormControl('', this.passwordMatch.bind(this))
-      }),
-      nom: new FormControl('', [this.validatorPaul, Validators.required], this.validatorAsync),
+      email: new FormControl(''),
+      hobbies: new FormArray([]),
+      password: new FormControl(''),
+      nom: new FormControl(''),
     });
+
+    console.log(this.form.controls);
+
+
   }
 
-  validatorPaul(formControl: AbstractControl): { [s: string]: boolean } | null {
-    if (formControl.value == 'paul') {
-      return { notPaul: true }
-    } else {
-      return null;
-    }
-  }
-
-  passwordMatch(formControl: AbstractControl): { [s: string]: boolean } | null {
-    if (formControl.value !== this.el.nativeElement.value) {
-      return { notMatching: true }
-    } else {
-      return null;
-    }
-  }
-
-  validatorAsync(formControl: AbstractControl): Promise<{ [s: string]: boolean } | null> {
-    return new Promise((resolve, rejet) => {
-      setTimeout(() => {
-        resolve(null);
-      }, 50000);
-    })
+  addHobby() {
+    this.hobbies.controls.push(new FormControl());
   }
 
   submit(): void {
