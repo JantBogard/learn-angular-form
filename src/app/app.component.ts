366da81041c7b4ec359966d8ebdd5ba0506cf206
JantBogard <joancenkonga@gmail.com>
2022-11-11 16:17:13 +0100

    form eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   public form: FormGroup;
+  private subscribe: Subscription = new Subscription();
 
   get hobbies() {
     return this.form.get('hobbies') as FormArray;
   }
 
+  constructor(private fb: FormBuilder) { }
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl(''),
-      hobbies: new FormArray([]),
-      password: new FormControl(''),
-      nom: new FormControl(''),
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      hobbies: this.fb.array([]),
+      password: [''],
+      nom: [''],
+      gender: [''],
+      city: [''],
+      majeur: ['']
     });
+
+    // this.form.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+
+    // this.form.statusChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+
+    // this.form = new FormGroup({
+    //   email: new FormControl(''),
+    //   hobbies: new FormArray([]),
+    //   password: new FormControl(''),
+    //   nom: new FormControl(''),
+    // });
 
     console.log(this.form.controls);
 
@@ -27,7 +52,11 @@ export class AppComponent implements OnInit {
   }
 
   addHobby() {
-    this.hobbies.controls.push(new FormControl());
+    const control = new FormControl();
+    this.subscribe = control.valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.hobbies.controls.push(control);
   }
 
   submit(): void {
